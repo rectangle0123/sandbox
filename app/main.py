0c2749bd1e5001e287303bcf1d7ef7bd046de023
rectangle0123 <rectangle0123@gmail.com>
2023-01-22 11:57:11 +0900

    DB接続をデコレータで実装

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ from utils import MemcachedUtils
 # セッション
 Session = sessionmaker(engine)
 
-
 def main():
     """ メインプログラム """
     try:
@@ -26,15 +25,23 @@ def main():
         print('\n')
         exit()
 
+def withsession(func):
+    """ DB接続デコレータ """
+    def wrapper(*args, **kwargs):
+        with engine.connect() as connection:
+            with Session(bind=connection) as session:
+                func(session)
+    return wrapper
 
-def search_by_name():
+@withsession
+def search_by_name(session: Session):
     """ 名前検索 """
     while True:
         name = input('名前検索: 名前を入力してください > ')
         # 入力がない場合は検索しない
         if not name:
             continue
-        # 入力があればキャッシュを探す
+        # キャッシュを探す
         cached = MemcachedUtils.get(name)
         if cached:
             print('DEBUG: Found in memcached.')
@@ -42,33 +49,31 @@ def search_by_name():
                 print(e)
             break
         # キャッシュになければ検索する
-        with engine.connect() as connection:
-            with Session(bind=connection) as session:
-                employees = session.query(Employee).filter(Employee.name.like(f'%\\{name}%', escape='\\')).all()
-                if not employees:
-                    print('該当するデータがありませんでした')
-                    continue
-                # キャッシュに登録
-                MemcachedUtils.set(name, employees)
-                for e in employees:
-                    print(e)
-                break
+        employees = session.query(Employee).filter(Employee.name.like(f'%\\{name}%', escape='\\')).all()
+        if not employees:
+            print('該当するデータがありませんでした')
+            continue
+        # キャッシュに登録
+        MemcachedUtils.set(name, employees)
+        for e in employees:
+            print(e)
+        break
 
-
-def search_by_years():
+@withsession
+def search_by_years(session: Session):
     """ 勤続年数 """
     while True:
         years = input('勤続年検索: 年数を入力してください > ')
         # 入力がない場合は検索しない
         if not years:
             continue
-        # タイムスタンプ変換
+        # 入力値をタイムスタンプに変換する
         try:
             threshold = (datetime.now() - relativedelta(years=int(years))).timestamp()
         except:
             print('年数を数字で入力してください')
             continue
-        # タイムスタンプに変換できればキャッシュを探す
+        # キャッシュを探す
         cached = MemcachedUtils.get(str(years))
         if cached:
             print('DEBUG: Found in memcached.')
@@ -76,17 +81,15 @@ def search_by_years():
                 print(e)
             break
         # キャッシュになければ検索する
-        with engine.connect() as connection:
-            with Session(bind=connection) as session:
-                employees = session.query(Employee).filter(Employee.joined_at <= threshold).all()
-                if not employees:
-                    print('該当するデータがありませんでした')
-                    continue
-                # キャッシュに登録
-                MemcachedUtils.set(str(years), employees)
-                for e in employees:
-                    print(e)
-                break
+        employees = session.query(Employee).filter(Employee.joined_at <= threshold).all()
+        if not employees:
+            print('該当するデータがありませんでした')
+            continue
+        # キャッシュに登録
+        MemcachedUtils.set(str(years), employees)
+        for e in employees:
+            print(e)
+        break
 
 
 if __name__ == '__main__':
